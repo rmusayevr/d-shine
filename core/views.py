1a078c65fd572e4e985b34dbadfdd6dae371946f
Rashad Musayev <rmusayevr19@gmail.com>
2023-11-25 15:16:31 +0400

    order

--- core/views.py
+++ core/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .forms import ContactForm
 from .models import Team
+from product.models import Product, Category
+from blog.models import Blog
+from django.db.models import Avg
 
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.annotate(avg_rating=Avg('product_review__rating')).order_by('-avg_rating')[:4]
+    categories = Category.objects.all()
+    blogs = Blog.objects.all().order_by('-created_at')[1:6]
+    last_blog = Blog.objects.last()
+    context = {
+        'products': products,
+        'categories': categories,
+        'blogs': blogs,
+        'last_blog': last_blog
+    }
+    return render(request, 'index.html', context=context)
 
 
 def about(request):
